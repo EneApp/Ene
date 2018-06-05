f30270b625c8b38b598861e0cb167510574a4967
Peijun Ma <peijun.ma@protonmail.com>
2018-06-05 23:23:49 +0800

    Implement OAuth

--- ene/api.py
+++ ene/api.py
@@ -13,3 +13,63 @@
 #
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import webbrowser
+from http.server import BaseHTTPRequestHandler, HTTPServer
+from threading import Thread
+from urllib.parse import urlencode
+
+CLIENT_ID = '584'
+REDIR_URI = 'http://127.0.0.1:50000'
+
+
+class OAuth:
+    TOKEN = None
+
+    def __init__(self, client_id, redirect_uri, addr, port):
+        server_addr = (addr, port)
+        self.httpd = HTTPServer(server_addr, RedirectHandler)
+        self.auth_params = {
+            'client_id': client_id,
+            'redirect_uri': redirect_uri,
+            'response_type': 'code'
+        }
+
+    @classmethod
+    def get_token(cls, client_id, redirect_uri, addr, port):
+        self = cls(client_id, redirect_uri, addr, port)
+        server_thread = Thread(target=self.httpd.serve_forever)
+        server_thread.start()
+        encode = urlencode(self.auth_params)
+        webbrowser.open(f'https://anilist.co/api/v2/oauth/authorize?{encode}')
+        while not cls.TOKEN:
+            pass
+        self.httpd.shutdown()
+        server_thread.join()
+        return cls.TOKEN if cls.TOKEN != -1 else None
+
+
+class RedirectHandler(BaseHTTPRequestHandler):
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+    def do_GET(self):
+        token = self.path.replace('/?code=', '')
+        if token:
+            self.send_response(200)  # OK
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Success!')
+            OAuth.TOKEN = token
+        else:
+            self.send_response(400)  # OK
+            self.send_header('Content-type', 'text/plain')
+            self.end_headers()
+            self.wfile.write(b'Fail!')
+            OAuth.TOKEN = -1
+
+
+class API:
+    def __init__(self):
+        self.token = OAuth.get_token(CLIENT_ID, REDIR_URI, '127.0.0.1', 50000)
