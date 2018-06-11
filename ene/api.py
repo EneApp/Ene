38fa55b8eb63b3bfa06643b94cea4698bee381bf
Peijun Ma <peijun.ma@protonmail.com>
2018-06-11 10:37:08 +0800

    Add graphql schema

--- ene/api.py
+++ ene/api.py
@@ -14,15 +14,30 @@
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
+from pathlib import Path
 from typing import Iterable, List, Optional
 
 from requests import HTTPError, post
 
 from ene.auth import OAuth
 from ene.errors import APIError
+from ene.util import load_file
 
 CLIENT_ID = 584
 GRAPHQL_URL = 'https://graphql.anilist.co'
+GRAPHQL_FILE_DIR = Path(__file__).parent.parent / 'graphql'
+
+
+def load_query(name: str) -> str:
+    """
+    Load a GraphQL query from disk
+
+    Args:
+        name: The filename of the query file
+
+    Returns: The query stored in the file
+    """
+    return load_file(GRAPHQL_FILE_DIR / name)
 
 
 class API:
@@ -38,13 +53,14 @@ class API:
             'Accept': 'application/json'
         }
 
-    def _request(self, query: str, variables: Optional[dict] = None) -> dict:
+    def query(self, query: str, variables: Optional[dict] = None, is_file=True) -> dict:
         """
         Makes HTTP request to the Anilist API
 
         Args:
             query: The GraphQL query to POST to the API
             variables: variables for the query, can be None
+            is_file: Wether the query is from a file
 
         Returns:
             The API response
@@ -52,6 +68,8 @@ class API:
         Raises:
             APIHTTPError if request failed
         """
+        if is_file:
+            query = load_query(query)
         post_json = {'query': query}
         if variables:
             post_json['variables'] = variables
@@ -64,43 +82,48 @@ class API:
         else:
             return res.json()
 
-    def _paged_request(self, query: str, variables: dict) -> Iterable[dict]:
+    def query_pages(self, query: str, variables: dict, is_file=True) -> Iterable[dict]:
+        """
+        Make paged requests to the Anilist API
+
+        Args:
+            query: The GraphQL query to POST to the API
+            variables: variables for the query, can be None
+            is_file: Wether the query is from a file
+
+        Returns:
+            A generator yielding each page of the API response
+
+        Raises:
+            APIHTTPError if request failed
+        """
         while True:
-            res = self._request(query, variables)
+            res = self.query(query, variables, is_file)
             has_next = res['data']['Page']['pageInfo']['hasNextPage']
-            yield res['data']
+            yield res
             if not has_next:
                 return
             variables['page'] += 1
 
     def get_season_animes(self, season: str, year: int, per_page: int,
                           sort: Optional[List[str]] = None) -> Iterable[dict]:
+        """
+        Query all anime of a given season
+
+        Args:
+            season: The season, one of: (WINTER, SPRING, SUMMER, FALL)
+            year: The year for the season
+            per_page: How many anime per page of the response
+            sort: List of keys to sort by, defaults to ID. See
+                https://anilist.github.io/ApiV2-GraphQL-Docs/mediasort.doc.html
+
+        Returns:
+            A generator yielding pages of anime in the season
+
+        Raises:
+            APIHTTPError if request failed
+        """
         sort = sort or ['ID']
-        query = """
-query ($page: Int, $perPage: Int, $season: MediaSeason, $seasonYear: Int, $sort: [MediaSort]) {
-    Page(page: $page, perPage: $perPage) {
-        pageInfo {
-            total
-            hasNextPage
-        }
-            media(season: $season, seasonYear: $seasonYear, type: ANIME, sort: $sort) {
-                id
-                title {
-                    userPreferred
-                }
-                averageScore
-                description
-                format
-                genres
-                coverImage {
-                    large
-                }
-                episodes
-                popularity
-        }
-    }
-}
-"""
         variables = {
             'page': 1,
             'perPage': per_page,
@@ -108,4 +131,4 @@ query ($page: Int, $perPage: Int, $season: MediaSeason, $seasonYear: Int, $sort:
             'seasonYear': year,
             'sort': sort
         }
-        return self._paged_request(query, variables)
+        return self.query_pages('season.graphql', variables)
