374a9f36b783ff3ec57433ffed846a9adc29ca68
Peijun Ma <peijun.ma@protonmail.com>
2018-06-11 16:46:06 +0800

    Hook up the ui files to source code

--- ene/app.py
+++ ene/app.py
@@ -15,18 +15,90 @@
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 import sys
+from contextlib import contextmanager
 from pathlib import Path
+from typing import Optional
 
-from PySide2.QtCore import QFile, Qt
-from PySide2.QtUiTools import QUiLoader
-from PySide2.QtWidgets import QApplication
+from PySide2 import QtUiTools
+from PySide2.QtCore import Qt, QFile
+from PySide2.QtWidgets import QApplication, QAction, QMainWindow, QWidget
 
-if __name__ == "__main__":
-    QApplication.setAttribute(Qt.ApplicationAttribute.AA_ShareOpenGLContexts)
-    app = QApplication(sys.argv)
-    file = QFile(str(Path(__file__).parent.parent / 'ui' / 'ene.ui'))
-    file.open(QFile.ReadOnly)
-    loader = QUiLoader()
-    window = loader.load(file)
-    window.show()
-    sys.exit(app.exec_())
+QApplication.setAttribute(Qt.ApplicationAttribute.AA_ShareOpenGLContexts)
+
+UI_DIR = Path(__file__).parent.parent / 'ui'
+APP_NAME = 'ENE'
+
+
+@contextmanager
+def open_ui_file(filename: str) -> QFile:
+    """
+    Context manager to open a Qt ui file
+    Args:
+        filename: Filename of the ui file
+
+    Returns:
+        The ui file opened, and closes the file afterwards
+    """
+    uifile = QFile(filename)
+    try:
+        uifile.open(QFile.ReadOnly)
+        yield uifile
+    finally:
+        uifile.close()
+
+
+def load_ui_widget(ui_file: str, parent: Optional[QWidget] = None) -> QWidget:
+    """
+    Load a ui wiget from file
+    Args:
+        ui_file: The ui file name
+        parent: The parent of that wiget
+
+    Returns:
+        The loaded ui wiget
+    """
+    loader = QtUiTools.QUiLoader()
+    with open_ui_file(ui_file) as uifile:
+        ui = loader.load(uifile, parent)
+    uifile.close()
+    return ui
+
+
+class MainForm(QMainWindow):
+    """
+    Main form of the application
+    """
+
+    def __init__(self):
+        """
+        Initialize the ui files for the application
+        """
+        super().__init__()
+        self.setWindowTitle(APP_NAME)
+        self.main_window = load_ui_widget(str(UI_DIR / 'ene.ui'))
+        self.main_window.setWindowTitle(APP_NAME)
+        self.prefences_window = load_ui_widget(str(UI_DIR / 'settings.ui'))
+        self.prefences_window.setWindowTitle('Prefences')
+        self.setup_children()
+
+    def setup_children(self):
+        """
+        Setup all the child wigets of the main window
+        """
+        self.act_prefences = self.main_window.findChild(QAction, '﻿action_prefences')
+        assert self.act_prefences
+        self.act_prefences.triggered.connect(self.prefences_window.show)
+
+    @classmethod
+    def launch(cls):
+        """
+        Launch the Application
+        """
+        app = QApplication([APP_NAME])
+        form = cls()
+        form.main_window.show()
+        sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    MainForm.launch()
