43cd27b9b96f93bae7656a9fdf3f190718a3bb27
Peijun Ma <peijun.ma@protonmail.com>
2018-08-18 23:22:11 -0400

    Add path constants

--- ene/constants.py
+++ ene/constants.py
@@ -16,6 +16,7 @@
 
 """This module contains common constants."""
 import sys
+from os import getenv
 from pathlib import Path
 
 IS_37 = sys.version_info >= (3, 7)
@@ -30,6 +31,38 @@ else:
 IS_WIN = sys.platform in ('win32', 'cygwin')
 IS_LINUX = sys.platform.startswith('linux')
 IS_MAC = sys.platform == 'darwin'
+
+_DATA_HOME = getenv('XDG_DATA_HOME')
+_CONFIG_HOME = getenv('XDG_CONFIG_HOME')
+_CACHE_HOME = getenv('XDG_CACHE_HOME')
+_HOME = Path.home()
+
+if _DATA_HOME:
+    DATA_HOME = Path(_DATA_HOME, 'ene')
+elif IS_WIN:
+    DATA_HOME = Path(getenv('APPDATA'), 'ene', 'data')
+elif IS_MAC:
+    DATA_HOME = _HOME / 'Library' / 'Application Support' / 'ene' / 'data'
+else:
+    DATA_HOME = _HOME / '.local' / 'share' / 'ene'
+
+if _CONFIG_HOME:
+    CONFIG_HOME = Path(_CONFIG_HOME, 'ene')
+elif IS_WIN:
+    CONFIG_HOME = Path(getenv('APPDATA'), 'ene', 'config')
+elif IS_MAC:
+    CONFIG_HOME = _HOME / 'Library' / 'Application Support' / 'ene' / 'config'
+else:
+    CONFIG_HOME = _HOME / '.config' / 'ene'
+
+if _CACHE_HOME:
+    CACHE_HOME = Path(_CACHE_HOME, 'ene')
+elif IS_WIN:
+    CACHE_HOME = Path(getenv('TEMP'), 'ene')
+elif IS_MAC:
+    CACHE_HOME = _HOME / 'Library' / 'Cache' / 'ene'
+else:
+    CACHE_HOME = _HOME / '.cache' / 'ene'
 
 APP_NAME = 'ENE'
 CLIENT_ID = 584
