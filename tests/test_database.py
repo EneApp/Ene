718b4f9bc7c8075f6b88aed74a18404f521b10af
Peijun Ma <peijun.ma@protonmail.com>
2018-08-22 19:09:08 -0400

    Fix?

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,11 +1,12 @@
-from ene.database import Database
 from pathlib import Path
-from . import rmdir
 from sqlite3 import DatabaseError
 
 import pytest
 
-DB_DIR = Path(__file__).parent / 'ene'
+from ene.database import Database
+from . import rmdir
+
+DB_DIR = Path(__file__).parent / 'db'
 DB_PATH = DB_DIR / 'ene.eb'
 
 
@@ -14,13 +15,13 @@ def empty_db():
     rmdir(DB_DIR, True)
     DB_DIR.mkdir(parents=True)
     db = Database(str(DB_PATH))
-    yield db
-    del db
-    rmdir(DB_DIR, True)
+    try:
+        yield db
+    finally:
+        del db
+        rmdir(DB_DIR, True)
 
 
 def test_database_not_setup(empty_db):
     with pytest.raises(DatabaseError):
         empty_db.get_all()
-
-
