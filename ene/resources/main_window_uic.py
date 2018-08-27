3fda15b8bfdc0a86dfcb0f612e8a6a7aa952c161
Peijun Ma <peijun.ma@protonmail.com>
2018-08-26 20:10:34 -0400

    Update resources

--- ene/resources/main_window_uic.py
+++ ene/resources/main_window_uic.py
@@ -3,7 +3,7 @@
 # Form implementation generated from reading ui file 'ene/resources/main_window.ui',
 # licensing of 'ene/resources/main_window.ui' applies.
 #
-# Created: Fri Aug 24 20:52:28 2018
+# Created: Sun Aug 26 17:17:24 2018
 #      by: pyside2-uic  running on PySide2 5.11.1
 #
 # WARNING! All changes made in this file will be lost!
